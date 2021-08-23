3f384a097e64459369f98aa49a4bf08d0ac070b8
Adam Hacker <adamhack2006@gmail.com>
2021-08-22 17:07:36 -0700

    Added comments to the AuthenticationViewModel

--- fbla-school/Utilities/Auth/AuthenticationViewModel.swift
+++ fbla-school/Utilities/Auth/AuthenticationViewModel.swift
@@ -11,51 +11,80 @@ import GoogleSignIn
 
 class AuthenticationViewModel: NSObject, ObservableObject {
     
+    // Stores and creates the Google Sign-In configuration value to be used throughout the Google Sign-In process
     let signInConfig = GIDConfiguration.init(clientID: (FirebaseApp.app()?.options.clientID!)!)
     
+    // A custom enum for defining sign-in state
     enum SignInState {
         case signedIn
         case signedOut
     }
     
+    // A published var for tracking sign-in state, conforms to the previously declared SignInState enum
     @Published var state: SignInState = .signedOut
     
+    // Handles the sign in process
     func signIn() {
+        
+        // Checks if there is a user logged in
         if GIDSignIn.sharedInstance.currentUser == nil {
             
+            // If no user is longed in, Google Sign-In starts the sign-in flow
             GIDSignIn.sharedInstance.signIn(with: signInConfig, presenting: (UIApplication.shared.windows.first?.rootViewController)!) { [weak self] user, error in
                 if error == nil {
-                    self?.firebaseAuthentication(withUser: user!)
+                    
+                    // If there is no error, sign the user in using Firebase
+                    self?.firebaseAuth(withUser: user!)
                 } else {
+                    
+                    // If there is an error, print the error out to the console
                     print(error.debugDescription)
                 }
             }
         }
     }
     
-    private func firebaseAuthentication(withUser user: GIDGoogleUser) {
+    // Takes in a user and signs them in with Firebase
+    private func firebaseAuth(withUser user: GIDGoogleUser) {
+    
+        // Stores the authentication information for the provided user
         let authentication = user.authentication
+        
+        // Creates a credential using the authentication information
         let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken!, accessToken: authentication.accessToken)
         
+        // Signs the user in with Firebase using the credential
         Auth.auth().signIn(with: credential) { (_, error) in
             if let error = error {
+                
+                // If there is an error, print the error out to the console
                 print(error.localizedDescription)
             } else {
+                
+                // User is logged in if there is no error automatically
+                
+                // Change the sign-in state to signedIn
                 self.state = .signedIn
             }
         }
-        
     }
     
-    // 5
+    // Signs out the user
     func signOut() {
+        
+        // Signs the user out of their Google account
         GIDSignIn.sharedInstance.signOut()
         
+        // Signs the user out of Firebase
         do {
-            try Auth.auth().signOut()
             
+            // Signs the user out and changes the state to signedOut
+            try Auth.auth().signOut()
             state = .signedOut
+            
         } catch let signOutError as NSError {
+            
+            // If there is an error, prints the error out to the console
             print(signOutError.localizedDescription)
         }
     }
